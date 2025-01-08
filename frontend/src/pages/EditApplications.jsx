78f2f05db867c135d019877f706e840c4b1bfc9f
Dorisa Shehi <doris.shehi2@gmail.com>
2025-01-07 20:17:37 -0500

    View and create module style added

--- frontend/src/pages/EditApplications.jsx
+++ frontend/src/pages/EditApplications.jsx
@@ -2,23 +2,41 @@ import { AuthenticatedContext } from "../context/AuthenticatedContext";
 import { useContext, useState } from "react";
 import Select from "react-select";
 import Modal from "react-modal";
-const Applications = () => {
+const EditApplications = () => {
   const { user, isAuthenticated } = useContext(AuthenticatedContext);
-  const [modalIsOpen, setIsOpen] = useState({
-    creation: false,
-    edition: false,
-    view: false,
-  });
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-  function openModal(modalName) {
-    setIsOpen({ ...modalIsOpen, [modalName]: true });
+  function openModal() {
+    setIsOpen(true);
   }
 
-  function closeModal(modalName) {
-    setIsOpen({ ...modalIsOpen, [modalName]: false });
+  function closeModal() {
+    setIsOpen(false);
   }
-
   Modal.setAppElement("#root");
+
+  const customStyles = {
+    // modal: {
+    //   position: "absolute",
+    //   inset: "40px",
+    //   border: "1px solid rgb(204, 204, 204)",
+    //   background: "rgb(255, 255, 255)",
+    //   overflow: "auto",
+    //   borderRadius: "4px",
+    //   outline: "none",
+    //   padding: "20px",
+    //   top: "50%",
+    //   left: "50%",
+    //   right: "auto",
+    //   bottom: "auto",
+    //   marginRight: "-50%",
+    //   transform: "translate(-50%, -50%)",
+    //   width: "80%", // Adjust width
+    // },
+    // overlay: {
+    //   backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent background
+    // },
+  };
 
   const locationOptions = [
     { value: "onsite", label: "On-Site" },
@@ -48,10 +66,6 @@ const Applications = () => {
   ];
 
   const [selectedMethod, setMethodSelected] = useState(null);
-
-  const hangeEditAction = () => {
-    console.log("Edit Action");
-  };
   return (
     <>
       <div className="container-main justify-center bg-[#f5f7f9]">
@@ -80,7 +94,7 @@ const Applications = () => {
               <div className="flex-1 py-10 flex flex-col">
                 <button
                   type="submit"
-                  onClick={() => openModal("creation")}
+                  onClick={() => openModal()}
                   className="main-btn float-right mt-0  mb-5 "
                 >
                   <p>Create</p>
@@ -118,12 +132,8 @@ const Applications = () => {
                       <td className="table-item">2025-01-05</td>
                       <td className="table-item">Pending</td>
                       <td className="table-item">
-                        <button
-                          onClick={() => openModal("view")}
-                          className="main-btn bg-lightGreen"
-                        >
-                          View
-                        </button>
+                        <button>Edit</button>
+                        <button>Delete</button>
                       </td>
                     </tr>
 
@@ -137,12 +147,8 @@ const Applications = () => {
                       <td className="table-item">2025-01-05</td>
                       <td className="table-item">Pending</td>
                       <td className="table-item">
-                        <button
-                          onClick={() => openModal("view")}
-                          className="main-btn bg-lightGreen"
-                        >
-                          View
-                        </button>
+                        <button>Edit</button>
+                        <button>Delete</button>
                       </td>
                     </tr>
                   </tbody>
@@ -153,234 +159,8 @@ const Applications = () => {
             <p>Not Authenticated</p>
           )}
           <Modal
-            isOpen={modalIsOpen.creation}
-            onRequestClose={() => closeModal("creation")}
-            style={{ customStyles }}
-            contentLabel="Create Application"
-          >
-            <div className="flex flex-row items-center justify-between px-[20px] pb-[10px] border-b-[2px] border-borderColor">
-              <h1 className="header-main flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 -960 960 960"
-                  className="icon-style-large"
-                  fill="#5f6368"
-                >
-                  <path d="M120-320v-80h280v80H120Zm0-160v-80h440v80H120Zm0-160v-80h440v80H120Zm520 480v-160H480v-80h160v-160h80v160h160v80H720v160h-80Z" />
-                </svg>
-                Application Details
-              </h1>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 -960 960 960"
-                className="icon-style-small cursor-pointer"
-                fill="#5f6368"
-                onClick={() => closeModal("creation")}
-              >
-                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-              </svg>
-            </div>
-
-            <form className="login-form flex w-fit flex-col">
-              <div className="flex flex-wrap gap-10">
-                <div className="login-input-container">
-                  <label htmlFor="company_name" className="login-label">
-                    Company Name<em className="text-redText">*</em>
-                  </label>
-                  <input
-                    type="text"
-                    id="company_name"
-                    name="company_name"
-                    placeholder="TechCorp Inc."
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container">
-                  <label htmlFor="company_web" className="login-label">
-                    Company Website<em className="text-redText">*</em>
-                  </label>
-                  <input
-                    type="text"
-                    id="company_web"
-                    name="company_web"
-                    placeholder="www.techcorp.com"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container flex items-start min-w-[300px] ">
-                  <label htmlFor="favorite" className="login-label ">
-                    Favorite
-                  </label>
-
-                  <input
-                    type="checkbox"
-                    id="favorite"
-                    name="favorite"
-                    className="login-input mr-[8px]"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="application_date" className="login-label">
-                    Apply Date<em className="text-redText">*</em>
-                  </label>
-                  <input
-                    type="date"
-                    id="application_date"
-                    name="application_date"
-                    placeholder="2024-01-15"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="application_method" className="login-label">
-                    Apply Method<em className="text-redText">*</em>
-                  </label>
-                  <Select
-                    defaultValue={selectedMethod}
-                    onChange={() => setMethodSelected}
-                    options={methodOptions}
-                    id="application_method"
-                    name="application_method"
-                    className="login-input modal-input p-[0px] outline-none active:outline-none"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="application_url" className="login-label">
-                    Apply URL<em className="text-redText">*</em>
-                  </label>
-                  <input
-                    type="text"
-                    id="application_url"
-                    name="application_url"
-                    placeholder="www.techcorp.com/careers"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="positon" className="login-label">
-                    Position<em className="text-redText">*</em>
-                  </label>
-                  <input
-                    type="text"
-                    id="positon"
-                    name="positon"
-                    placeholder="Software Engineer"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="location" className="login-label">
-                    Location<em className="text-redText">*</em>
-                  </label>
-                  <Select
-                    defaultValue={selectedLocation}
-                    onChange={() => setLocationSelected}
-                    options={locationOptions}
-                    id="location"
-                    name="location"
-                    className="login-input modal-input p-[0px] outline-none active:outline-none"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="interview_date" className="login-label">
-                    Interview Date
-                  </label>
-                  <input
-                    type="date"
-                    id="interview_date"
-                    name="interview_date"
-                    placeholder="2024-01-20"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="amount_offered" className="login-label">
-                    Offer Amount
-                  </label>
-                  <input
-                    type="number"
-                    id="amount_offered"
-                    name="amount_offered"
-                    placeholder="85000"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="rejected" className="login-label">
-                    Rejected
-                  </label>
-                  <Select
-                    defaultValue={selectedRejection}
-                    onChange={() => setRejectedSelected}
-                    options={rejectedOptions}
-                    id="rejected"
-                    name="rejected"
-                    className="login-input modal-input p-[0px] outline-none active:outline-none"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="contact_name" className="login-label">
-                    Contact Name
-                  </label>
-                  <input
-                    type="text"
-                    id="contact_name"
-                    name="contact_name"
-                    placeholder="John Doe"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="notes" className="login-label">
-                    Notes
-                  </label>
-                  <textarea
-                    id="notes"
-                    name="notes"
-                    rows={5}
-                    placeholder="The interview went well, waiting for feedback."
-                    className="login-input modal-input"
-                  ></textarea>
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="contact_email" className="login-label">
-                    Contact Email
-                  </label>
-                  <input
-                    type="email"
-                    id="contact_email"
-                    name="contact_email"
-                    placeholder="john.doe@example.com"
-                    className="login-input modal-input"
-                  />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="company_phone" className="login-label">
-                    Company Phone
-                  </label>
-                  <input
-                    type="text"
-                    id="company_phone"
-                    name="company_phone"
-                    placeholder="(555) 123-4567"
-                    className="login-input modal-input"
-                  />
-                </div>
-              </div>
-
-              <button
-                type="submit"
-                className="main-btn float-right mt-0 self-end"
-                // onClick={(e) => submitAction(e)}
-              >
-                Create
-              </button>
-            </form>
-          </Modal>
-
-          <Modal
-            isOpen={modalIsOpen.view}
-            onRequestClose={() => closeModal("view")}
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
             style={{ customStyles }}
             contentLabel="Edit Application"
           >
@@ -399,9 +179,9 @@ const Applications = () => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 -960 960 960"
-                className="icon-style-small cursor-pointer"
+                className="icon-style-small"
                 fill="#5f6368"
-                onClick={() => closeModal("view")}
+                onClick={closeModal}
               >
                 <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
               </svg>
@@ -463,7 +243,7 @@ const Applications = () => {
                   </label>
                   <Select
                     defaultValue={selectedMethod}
-                    onChange={() => setMethodSelected}
+                    onChange={setMethodSelected}
                     options={methodOptions}
                     id="application_method"
                     name="application_method"
@@ -500,7 +280,7 @@ const Applications = () => {
                   </label>
                   <Select
                     defaultValue={selectedLocation}
-                    onChange={() => setLocationSelected}
+                    onChange={setLocationSelected}
                     options={locationOptions}
                     id="location"
                     name="location"
@@ -537,7 +317,7 @@ const Applications = () => {
                   </label>
                   <Select
                     defaultValue={selectedRejection}
-                    onChange={() => setRejectedSelected}
+                    onChange={setRejectedSelected}
                     options={rejectedOptions}
                     id="rejected"
                     name="rejected"
@@ -555,18 +335,6 @@ const Applications = () => {
                     placeholder="John Doe"
                     className="login-input modal-input"
                   />
-                </div>
-                <div className="login-input-container w-[1/3]">
-                  <label htmlFor="notes" className="login-label">
-                    Notes
-                  </label>
-                  <textarea
-                    id="notes"
-                    name="notes"
-                    rows={5}
-                    placeholder="The interview went well, waiting for feedback."
-                    className="login-input modal-input"
-                  ></textarea>
                 </div>
                 <div className="login-input-container w-[1/3]">
                   <label htmlFor="contact_email" className="login-label">
@@ -592,7 +360,27 @@ const Applications = () => {
                     className="login-input modal-input"
                   />
                 </div>
+                <div className="login-input-container w-[1/3]">
+                  <label htmlFor="notes" className="login-label">
+                    Notes
+                  </label>
+                  <textarea
+                    id="notes"
+                    name="notes"
+                    rows={5}
+                    placeholder="The interview went well, waiting for feedback."
+                    className="login-input modal-input"
+                  ></textarea>
+                </div>
               </div>
+
+              <button
+                type="submit"
+                className="main-btn float-right mt-0"
+                // onClick={(e) => submitAction(e)}
+              >
+                Create
+              </button>
             </form>
           </Modal>
         </div>
@@ -600,4 +388,4 @@ const Applications = () => {
     </>
   );
 };
-export default Applications;
+export default EditApplications;
